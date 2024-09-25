c227f60104db0736170bbb783cadd3871210cca1
skald1311 <hoangmanhduong01@gmail.com>
2024-09-25 11:57:02 -0600

    feat: add visualization of decision tree

--- dev_experiments/gbt.py
+++ dev_experiments/gbt.py
@@ -1,11 +1,14 @@
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor  # Or GradientBoostingClassifier for classification
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 
-# Load the data for one of your x and y
-x = pd.read_csv('x_AT1G01060.csv', index_col=0)
-y = pd.read_csv('y_AT1G01060.csv', index_col=0).squeeze()
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+# Load the data for one of x and y
+x = pd.read_csv('x_AT1G51140.csv', index_col=0)
+y = pd.read_csv('y_AT1G51140.csv', index_col=0).squeeze()
 
 # Split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
@@ -14,16 +17,20 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
 model.fit(x_train, y_train)
 
+# Visualization of decision tree
+# estimator = model.estimators_[0, 0]  # [1, 0] for 2nd tree, and so on...
+# plt.figure(figsize=(20, 10))
+# plot_tree(estimator, filled=True, feature_names=x_train.columns)
+# plt.show()
+
 # Get feature importances
 feature_importances = model.feature_importances_
 
-# Create a DataFrame for easy viewing and saving
 feature_importance_df = pd.DataFrame({
     'Feature': x.columns,
     'Importance': feature_importances
 })
 
-# Optionally, print the results
 target_gene_name = "y_AT1G01060.csv".split('_')[1].split('.')[0]  # Extract gene name from file
 print(f"Feature Importances for {target_gene_name}:")
 print(feature_importance_df)
