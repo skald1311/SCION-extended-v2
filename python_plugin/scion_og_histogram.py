dcc17f581bc0e2c396b11f428d992cb1c5386fc7
skald1311 <hoangmanhduong01@gmail.com>
2024-10-24 15:27:10 -0600

    feat: add original rf histogram

---
+++ python_plugin/scion_og_histogram.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the CSV file
+df = pd.read_csv("FINAL_NETWORK.txt", sep='\t')
+
+# Create a histogram of the 'Importance' column
+plt.figure(figsize=(8,6))
+plt.hist(df['Weight'], bins=10, edgecolor='black')
+
+plt.xlabel('Edge Weight')
+plt.ylabel('Frequency')
+plt.title('Histogram of Edge Weight')  # Feature Importance == Edge weight
+
+plt.savefig("hist_og_scion.png", dpi=300)  # You can adjust dpi for resolution
+plt.show()
